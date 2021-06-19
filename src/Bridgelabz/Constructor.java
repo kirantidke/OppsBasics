c8424041284a3406aedd1bb2c3fe641e069d02e2
kirantidke <kirantidke2013@.com>
2021-06-19 18:27:37 +0530

    "[kiran] added file to new branch"

---
+++ src/Bridgelabz/Constructor.java
@@ -0,0 +1,13 @@
+package Bridgelabz;
+
+public class Constructor {
+	int x;
+	public Constructor(int y) {
+		x = y;
+	}
+	public static void main(String[] args) {
+		Constructor c1 = new Constructor(5);
+		System.out.println(c1.x);
+		}
+
+}
